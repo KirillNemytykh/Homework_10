485853103bbe485c568b04a9b6527e1cbfe2789a
Kirill Nemytykh <nemytykhkirill@gmail.com>
2024-01-15 23:12:35 +0700

    Added PosterManager

---
+++ src/main/java/ru/netology/javaqa/posterManager.java
@@ -0,0 +1,39 @@
+public class posterManager {
+    private String[] moviesList = new String[0];
+    private int limit;
+
+    public posterManager(int limit) {
+        this.limit = limit;
+    }
+    public posterManager() {
+        limit = 5;
+    }
+
+    public void addFilm(String movie) {
+        String[] tmp = new String[moviesList.length + 1];
+        for (int i = 0; i < moviesList.length; i++) {
+            tmp[i] = moviesList[i];
+        }
+        tmp[tmp.length -1] = movie;
+        moviesList = tmp;
+    }
+
+    public String[] findAll() {
+        return moviesList;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (limit < moviesList.length) {
+            resultLength = limit;
+        }
+        else {
+            resultLength = moviesList.length;
+        }
+        String[] result = new String[resultLength];
+        for (int i = 0; i < resultLength; i++){
+            result[i] = moviesList[moviesList.length - 1 - i];
+        }
+        return result;
+    }
+}
